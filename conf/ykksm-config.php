44b24d00151faea76bc13e7578a8894d9fe8359c
Michal Němec <michal.nemec.dev@gmail.com>
2021-05-18 21:00:36 +0200

    Fix missing env changes

--- conf/ykksm-config.php
+++ conf/ykksm-config.php
@@ -2,7 +2,7 @@
 $dbuser = getenv('YKKSM_READER_DB_USER_NAME');
 $dbpass = getenv('YKKSM_READER_DB_USER_PASSWORD');
 $basepath = '';
-$dbname = 'ykksm';
+$dbname = getenv('YKKSM_DB_NAME');
 $dbserver = getenv('MYSQL_HOST');
 $dbport = getenv('MYSQL_PORT');
 $dbtype = 'mysql';
